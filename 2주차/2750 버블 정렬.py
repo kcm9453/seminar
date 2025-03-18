b51600cf0d9fbb2eef6f4ea2a695ae2da02b2414
창민 강 <kcm9453@naver.com>
2025-03-18 23:16:51 +0900

    seminar

--- 2주차/2750 버블 정렬.py
+++ 2주차/2750 버블 정렬.py
@@ -1,13 +1,13 @@
-N = int(input())  # 숫자의 개수를 입력받음
-numbers = [int(input()) for _ in range(N)]  # 숫자들을 리스트에 입력받음
+N = int(input())  # 사용자로부터 숫자의 개수를 입력 받음
+numbers = [int(input()) for _ in range(N)]  # 사용자로부터 숫자들을 입력받아 리스트에 저장
 
 # 버블 정렬(Bubble Sort) 알고리즘을 사용하여 리스트를 정렬
 # 리스트의 길이 - 1 번 반복하여 정렬을 수행
+# 이는 각 반복마다 가장 큰 숫자가 정렬된 위치로 이동하기 때문에 점점 반복 횟수가 줄어든다
 for i in range(len(numbers) - 1):
-    # 각 반복마다 현재 원소와 그 다음 원소를 비교하여 정렬
-    for j in range(len(numbers) - 1 - i):
+    for j in range(len(numbers) - 1 - i): # 두번째 for루프에서 리스트를 한번 순회하여 인접한 원소들과 비교
         if numbers[j] > numbers[j + 1]:
-            # 인접한 두 원소를 비교하여 순서가 뒤바뀌어 있다면 서로 위치를 변경
+            # 조건문에서 현재 원소와 다음 원소를 비교하여 정렬이 필요한 경우 위치를 교환한다.
             numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
 
 # 정렬된 숫자들을 순서대로 출력
--- 2주차/2750 삽입 정렬.py
+++ 2주차/2750 삽입 정렬.py
@@ -1,15 +1,15 @@
-N = int(input())
-nums = []
+N = int(input()) # 정렬한 숫자의 개수를 입력받음
+nums = [] # 숫자를 저장할 빈 리스트 생성
 
-for _ in range(N):
+for _ in range(N): # 반복문을 통해 사용자로부터 n개의 숫자를 입력 받아 num 리스트에다가 저장
     nums.append(int(input()))
 
 # Insert Sort
-for i in range(1, len(nums)):
-    while (i > 0) & (nums[i] < nums[i - 1]):
-        nums[i], nums[i - 1] = nums[i - 1], nums[i]
+for i in range(1, len(nums)): # 두 번째 원소로부터 i = 1로 시작하여 리스트 끝까지 반복
+    while (i > 0) and (nums[i] < nums[i - 1]): # 현재 숫자 num[i]가 이전 숫자 num[i-1]보다 작다면 자리를 교환함
+        nums[i], nums[i - 1] = nums[i - 1], nums[i] # 이 부분이 실행되어 작은 숫자가 앞으로 이동하게됨
 
-        i -= 1
+        i -= 1 # 실행되어 한 칸 더 왼쪽으로 이동하면서 계속 정렬 진행
 
 for n in nums:
-    print(n)
+    print(n) # 정렬이 완료되면 리스트를 차례대로 출력해준다
