21a59dbaef7fbe3d4cd0bedfcd963fe88bcf3ce0
창민 강 <kcm9453@naver.com>
2025-06-10 19:56:21 +0900

    seminar

--- 9주차/1197 내가 풀으려고 한 파일.py
+++ 9주차/1197 내가 풀으려고 한 파일.py
@@ -1,5 +1,30 @@
 import sys
 input = sys.stdin.readline()
 
-V, E = map(int,input().split())
+V, E = map(int,input().split()) # 정점(V)와 간선(E)듸 개수를 불러옴
+parent = [i for i in range(V + 1)] # 부모 테이블(자기 자신을 초기화 해줌)
+
+def parent_find(parent, x):
+    if parent[x] != x:
+        parent[x] = parent_find(parent, parent[x])
+        return parent[x]
+
+def union_find(parent, a, b):
+    root_a = union_find(parent, a)
+    root_b = union_find(parent, b)
+    if root_a < root_b:
+        parent[root_a] = b
+    else:
+        parent[root_b] = a
+
+edges = [] # 간선의 개수를 리스트 안에 저장
+
+for _ in range(E):
+    a, b, cost = map(int,sys.stdin.readline().split())
+    edges.append()
+
+edges.sort()
+
+
+
 
